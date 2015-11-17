f34756e86fd24768f7da142a497bf339208e47bb
Ryan Williams <rwilliams@rapid7.com>
2015-11-17 12:38:36 +0000

    Adding pom updates and connection class

---
+++ src/main/java/com/rapid7/graph/Connetion.java
@@ -0,0 +1,23 @@
+package com.rapid7.graph;
+
+import com.thinkaurelius.titan.core.TitanFactory;
+import com.thinkaurelius.titan.core.TitanGraph;
+import org.apache.commons.configuration.BaseConfiguration;
+
+/**
+ * Connection class for connecting to Titan instance.
+ */
+public class Connetion
+{
+   /**
+    * Sets the backend connection and uses the TitanFactory to create connection.
+    */
+   public void connect()
+   {
+      BaseConfiguration baseConfiguration = new BaseConfiguration();
+      baseConfiguration.setProperty("storage.backend", "cassandra");
+      baseConfiguration.setProperty("storage.hostname", "192.168.1.10");
+
+      TitanGraph titanGraph = TitanFactory.open(baseConfiguration);
+   }
+}
